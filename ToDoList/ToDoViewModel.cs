8cf6f16438c7b72c446708da8a6688face3529de
Ranir <arina2005vvv@gmail.com>
2025-11-14 17:43:13 +0500

    done

---
+++ ToDoList/ToDoViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToDoList
+{
+    public class ToDo
+    {
+        public bool Complete { get; set; } 
+         
+        public string Task { get; set; }
+        public ToDo(string task, bool complete) { Task = task; Complete = complete; }
+
+    }
+
+    public class ToDoViewModel : INotifyPropertyChanged
+    {
+        private string task = "";
+        private bool complete = false;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand AddTask {  get; set; }
+        public ICommand RemoveTask { get; set; }
+        public ObservableCollection<ToDo> ToDoList { get; } = new();
+
+        public ToDoViewModel()
+        {
+            AddTask = new Command(() =>
+            {
+                if (!string.IsNullOrWhiteSpace(Task))
+                {
+                    ToDoList.Add(new ToDo(Task, false));
+                    Task = "";
+                }
+            },
+            () => !string.IsNullOrWhiteSpace(Task));
+
+            RemoveTask = new Command<ToDo>((ToDo todo) =>
+            {
+                ToDoList.Remove(todo);
+            });
+        }
+
+        public string Task
+        {
+            get => task;
+            set
+            {
+                if (task != value)
+                {
+                    task = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public bool Complete
+        {
+            get => complete;
+            set
+            {
+                if (complete != value)
+                {
+                    complete = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(TextDecorations));
+                }
+            }
+        }
+
+        public TextDecorations TextDecorations => Complete ? TextDecorations.Strikethrough : TextDecorations.None;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            ((Command)AddTask).ChangeCanExecute();
+        }
+    }
+}
